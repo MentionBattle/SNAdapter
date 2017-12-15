34f777a5f1b790c2f7cd921d1ab202c8622e4549
Lebedev Roman <lebedeffroman@gmail.com>
2017-12-15 03:15:31 +0300

    Remove console logger

--- src/main/kotlin/org/mentionbattle/snadapter/impl/socialnetworks/handlers/VkHandler.kt
+++ src/main/kotlin/org/mentionbattle/snadapter/impl/socialnetworks/handlers/VkHandler.kt
@@ -1,10 +1,12 @@
 package org.mentionbattle.snadapter.impl.socialnetworks.handlers
 
+import org.apache.logging.log4j.LogManager
 import org.mentionbattle.snadapter.api.core.SocialNetwork
 import org.mentionbattle.snadapter.api.core.eventsystem.Event
 import org.mentionbattle.snadapter.api.core.eventsystem.EventHandler
 import org.mentionbattle.snadapter.api.core.socialnetworks.SocialNetworkHandler
 import org.mentionbattle.snadapter.impl.eventsystem.ExitEvent
+import org.mentionbattle.snadapter.impl.eventsystem.MentionEvent
 import org.mentionbattle.snadapter.impl.eventsystem.PrimitiveEventQueue
 import org.mentionbattle.snadapter.impl.socialnetworks.handlers.vk.VkStreamingService
 import org.mentionbattle.snadapter.impl.socialnetworks.handlers.vk.handlers.EventQueueHandler
@@ -15,6 +17,7 @@ import org.mentionbattle.snadapter.impl.socialnetworks.initalizers.VkServiceAuth
 @SocialNetwork("vk")
 internal class VkHandler(auth: VkServiceAuth, tags: Tags, eventQueue: PrimitiveEventQueue)
     : SocialNetworkHandler, EventHandler {
+    private val logger = LogManager.getLogger()
 
     var isWorking = true
     val eventQueue = eventQueue
@@ -27,13 +30,14 @@ internal class VkHandler(auth: VkServiceAuth, tags: Tags, eventQueue: PrimitiveE
                 vk.close()
                 isWorking = false
             }
+            is MentionEvent -> logger.info(event)
         }
     }
 
     override fun processData() {
         eventQueue.addHandler(this)
 
-        vk.addMsgHandler(LogHandler())
+//        vk.addMsgHandler(LogHandler())
         vk.addMsgHandler(EventQueueHandler(eventQueue))
 
         vk.ensureRules()
